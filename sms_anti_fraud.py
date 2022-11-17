bd0c0f7e2978da0b2ebd8ec3e5c28f3f09645f9e
kairkhan24 <999.9plus0.1@gmail.com>
2022-11-17 18:54:33 +0600

    modify limit logic and add some tests

--- sms_anti_fraud.py
+++ sms_anti_fraud.py
@@ -5,6 +5,7 @@ import redis
 import settings
 from django.apps import apps
 from sentry_sdk import capture_message
+from datetime import datetime
 
 
 r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
@@ -15,7 +16,10 @@ class SmsNotSentException(Exception):
 
 
 class MobizonSmsService:
+    REDIS_KEY_PREFIX = 'sms_limit_'
+
     def send_message(self, recipient_phone, text):
+
         if not settings.SEND_SMS:
             return
 
@@ -25,8 +29,10 @@ class MobizonSmsService:
             return
 
         sms_limit_enable = apps.get_model('hrm.AdminSettings').objects.get_sms_limit_enable()
+
+        r_key = f'{self.REDIS_KEY_PREFIX}{recipient_phone}'
         if sms_limit_enable:
-            if self.check_limit(recipient_phone=recipient_phone):
+            if self.check_limit(r_key=r_key):
                 return
 
         url = f'{settings.MOBIZON_URL}?recipient={recipient_phone}&text={text}&apiKey={settings.MOBIZON_KEY}&from=easytap'
@@ -38,25 +44,36 @@ class MobizonSmsService:
             raise SmsNotSentException()
 
         if sms_limit_enable:
-            self.rate_limit(recipient_phone=recipient_phone)
+            self.rate_limit(r_key=r_key)
         return got.json()
 
-    def check_limit(self, recipient_phone: str) -> bool:
-        if r.get(recipient_phone):
+    def check_limit(self, r_key: str) -> bool:
+        if r.get(r_key):
             sms_limit_count_per_user = apps.get_model('hrm.AdminSettings').objects.get_sms_limit_count_per_user()
-            current_count = int(r.get(recipient_phone))
+            sms_limit_period_in_seconds = apps.get_model('hrm.AdminSettings').objects.get_sms_limit_period_in_seconds()
+            current_count = int(r.get(r_key))
+
+            first_request_time = r.get(f'{r_key}_first_request_time').decode('utf-8')
+            first_request_dt = datetime.strptime(first_request_time, '%Y-%m-%d %H:%M:%S')
+            now_dt = datetime.now()
+
+            diff_dt = now_dt - first_request_dt
+            if diff_dt.total_seconds() >= sms_limit_period_in_seconds:
+                r.delete(r_key)
+                r.delete(f'{r_key}_first_request_time')
+                return False
+
             if current_count >= sms_limit_count_per_user:
                 return True
         return False
 
-    def rate_limit(self, recipient_phone: str):
-        if not r.get(recipient_phone):
-            sms_limit_period_in_seconds = apps.get_model('hrm.AdminSettings').objects.get_sms_limit_period_in_seconds()
-            r.set(recipient_phone, 1, ex=int(sms_limit_period_in_seconds))
+    def rate_limit(self, r_key: str):
+        if not r.get(r_key):
+            r.set(r_key, 1)
+            first_request_time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+            r.set(f'{r_key}_first_request_time', first_request_time)
         else:
-            current_count = int(r.get(recipient_phone))
-            expired_period_in_seconds = r.ttl(recipient_phone)
-            r.set(recipient_phone, current_count + 1, ex=expired_period_in_seconds)
+            r.incr(r_key)
 
 
 mobizon_sms_service = MobizonSmsService()
--- test_sms_anti_fraud.py
+++ test_sms_anti_fraud.py
@@ -1,30 +1,87 @@
 import unittest
+import redis
 import responses
 import settings
 from unittest import mock
-from sms_anti_fraud import mobizon_sms_service
+from datetime import datetime
+from sms_anti_fraud import mobizon_sms_service, SmsNotSentException
+
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+
+
+class MockModel:
+    class MockManager:
+        def get_not_send_sms_phone_numbers(self):
+            return [1, 2, 3]
+
+        def get_sms_limit_enable(self):
+            return True
+
+        def get_sms_limit_count_per_user(self):
+            return 10
+
+        def get_sms_limit_period_in_seconds(self):
+            return 300
+
+    objects = MockManager()
 
 
 class TestMobizonSmsService(unittest.TestCase):
     @responses.activate
     @mock.patch('django.apps.apps.get_model')
     def test_send_message(self,
-                          # mock_requests,
                           mock_model,
                           ):
         responses.add(responses.GET, f'{settings.MOBIZON_URL}?recipient=+77051234567&text=Hello, its test.&apiKey={settings.MOBIZON_KEY}&from=easytap',
                       json={'data': {"messageId": "123456"}}, status=200)
 
-        mock_model.objects.get_not_send_sms_phone_numbers.return_value = [1, 2, 3]
-        mock_model.objects.get_sms_limit_enable.return_value = True
-        mock_model.objects.get_sms_limit_count_per_user.return_value = 10
-        mock_model.objects.get_sms_limit_period_in_seconds.return_value = 120
+        mock_model.return_value = MockModel()
 
         result = mobizon_sms_service.send_message(
             recipient_phone="+77051234567",
             text="Hello, its test."
         )
         self.assertEqual(result.get('data').get('messageId'), "123456", "Message not send")
+
+    @mock.patch('django.apps.apps.get_model')
+    def test_send_message_error(self, mock_model):
+
+        mock_model.return_value = MockModel()
+
+        with self.assertRaises(SmsNotSentException):
+            result = mobizon_sms_service.send_message(
+                recipient_phone="+770522",
+                text="Hello, its test."
+            )
+
+    @mock.patch('django.apps.apps.get_model')
+    def test_check_limit_not_exists(self, mock_model):
+        mock_model.return_value = MockModel()
+
+        r.delete('keykey1')
+
+        result = mobizon_sms_service.check_limit(r_key="keykey1")
+        self.assertEqual(result, False)
+
+    @mock.patch('django.apps.apps.get_model')
+    def test_check_limit_with_gte_period(self, mock_model):
+        mock_model.return_value = MockModel()
+
+        r.set('keykey2', 50)
+        test_first_request_time = '2022-11-11 17:00:00'
+        r.set('keykey2_first_request_time', test_first_request_time)
+        result = mobizon_sms_service.check_limit(r_key="keykey2")
+        self.assertEqual(result, False)
+
+    @mock.patch('django.apps.apps.get_model')
+    def test_check_limit_with_lte_period(self, mock_model):
+        mock_model.return_value = MockModel()
+
+        r.set('keykey3', 50)
+        test_first_request_time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+        r.set('keykey3_first_request_time', test_first_request_time)
+        result = mobizon_sms_service.check_limit(r_key="keykey3")
+        self.assertEqual(result, True)
 
 
 if __name__ == '__main__':
